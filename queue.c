2966f2ec3a9392b204598c2ab6dd127ec75397a9
Edwin Wu <thwuedwin@gmail.com>
2025-03-05 20:32:24 +0800

    Implement q_remove_head and q_remove_tail
    The difference between these two functions is the position of removal.
    If @head is NULL, return immediately.
    If string pointer is not NULL, copy the data from the entry within
    the buffer size. Otherwise, skip the copy operation.
    Finally, remove the entry, reconnect the queue, initialize the entry,
    and return its reference.
    
    Change-Id: I9d56531b22f168ed1949737cebc4e364073ff584

--- queue.c
+++ queue.c
@@ -90,13 +90,43 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head)) {
+        return NULL;
+    }
+
+    element_t *element = list_first_entry(head, element_t, list);
+    if (sp) {
+        strncpy(sp, element->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    struct list_head *next = element->list.next;
+    head->next = next;
+    next->prev = head;
+    INIT_LIST_HEAD(&(element->list));
+
+    return element;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head)) {
+        return NULL;
+    }
+
+    element_t *element = list_last_entry(head, element_t, list);
+    if (sp) {
+        strncpy(sp, element->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    struct list_head *prev = element->list.prev;
+    head->prev = prev;
+    prev->next = head;
+    INIT_LIST_HEAD(&(element->list));
+
+    return element;
 }
 
 /* Return number of elements in queue */
